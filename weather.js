499c3fcbb820519c47672d1a15e7b300c3a26089
willarmy20 <81721198+willarmy20@users.noreply.github.com>
2021-08-27 13:09:21 -0400

    added icons to weather app

--- weather.js
+++ weather.js
@@ -2,29 +2,24 @@ const input = document.querySelector('#city');
 const button = document.getElementById('submit').addEventListener('click', userInput);
 
 
-function userInput(){
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${APIKEY}`)
-.then(response => response.json())
-.then(data => {
-    console.log(input.value);
-    console.log(data);
-    // console.log(data.list.main.temp);
 
-    let kelvin = data.list[0].main.temp;
-    let degC = Math.floor(kelvin - 273.15);
+async function userInput(){
+const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${APIKEY}`;
+const weatherData = await fetch (weatherURL);
+const weatherJson = await weatherData.json();
+console.log(weatherJson)
 
-    console.log(degC);
+const kelvin = weatherJson.list[0].main.temp;
+const degC = Math.floor(kelvin - 273.15);
 
-    //F = C * 1.8 + 32;
-    let degF = Math.floor(degC * 1.8 +32);
-    console.log(degF);
+//F = C * 1.8 + 32;
+const degF = Math.floor(degC * 1.8 +32);
+console.log(degF);
 
-    let div = document.querySelector('#result');
-        // div.innerHTML = `<h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${degF.toString()}°F <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        // <small>${data.weather[0].main}</small>`;
-        div.textContent = `${degF.toString()} F°`
-        input.value = "";
-})
+const div = document.querySelector('#result');
+div.innerHTML = `<h2><img src="https://openweathermap.org/img/wn/${weatherJson.list[0].weather[0].icon}@2x.png" /> ${degF.toString()}°F <img src="https://openweathermap.org/img/wn/${weatherJson.list[0].weather[0].icon}@2x.png" /></h2>
+<small>${weatherJson.list[0].weather[0].main}</small>`;
+input.value = "";
 }
 
 
